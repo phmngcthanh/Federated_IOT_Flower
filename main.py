46a2a56cb69105013e2640b433fc959aba1ddef5
Thanh Pham Ngoc <19520958@gm.uit.edu.vn>
2022-12-21 01:40:32 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#   BSD 3-clause License
+#   Copyright (c) 2022. Thanh Pham Ngoc <phmngcthanh <AT>gmail.com>
+#   All rights reserved.
+#   A copy of the license can be found at the LICENSE file, or the link https://github.com/phmngcthanh/jetson-net/blob/master/LICENSE
+#
+#  IOT Federated Learning guided by Assoc.Prof. Le Trung Quan#
+#  This is Python part.#
+#  19520958  - Thanh Pham-Ngoc #
+#  19520262  - Pham Nguyen Viet Tan #
+#
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
